c98b3228b801aeae56ed7a0cd1fe595bb0d3490c
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2019-02-21 14:58:44 +0100

    1.1.2 Data loaded

--- src/main.py
+++ src/main.py
@@ -7,9 +7,12 @@ Email: fahadalisarwar@gmail.com
 
 '''
 ########################################################################################################################
+
 # imports
 from preparation.load_mach_data import *
 from preparation.load_sensor_data import *
+from preparation.load_mach_fail import *
+from preparation.load_error_data import *
 
 
 # The first step would be loading data
--- src/preparation/load_sensor_data.py
+++ src/preparation/load_sensor_data.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 print("*******************************loading data from Files*******************************")
 
-data = pd.read_csv('G:/projects/ml-project/data/raw/telemetry.csv')
-sens_date = data.iloc[:, 0].values
-sens_id = data.iloc[:, 1].values
-sens_volt = data.iloc[:, 2].values
-sens_rot = data.iloc[:, 3].values
-sens_pres = data.iloc[:, 4].values
-sens_vib = data.iloc[:, 5].values
+data_sens = pd.read_csv('G:/projects/ml-project/data/raw/telemetry.csv')
+sens_date = data_sens.iloc[:, 0].values
+sens_id = data_sens.iloc[:, 1].values
+sens_volt = data_sens.iloc[:, 2].values
+sens_rot = data_sens.iloc[:, 3].values
+sens_pres = data_sens.iloc[:, 4].values
+sens_vib = data_sens.iloc[:, 5].values
 
 
 def main():
