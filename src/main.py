bca56aab99fe9b2468c703386e68ad6fc47cdbf8
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2019-02-21 11:35:59 +0100

    1.1.1
    testing on files

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sys
+import file2 as f
+import myfile as mf
+import preparation.pre as p
+print(sys.path)
+
+sys.path.append('G:/projects/ml-project/src')
+np.arange(4)
+
+print(mf.num1, f.num2, p.n)
+
+
+
+
